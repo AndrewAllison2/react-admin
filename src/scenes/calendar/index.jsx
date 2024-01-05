95688ffc051e8872b5c8d1db7267597d4f4c26be
Andrew Allison <andrewallison5211@gmail.com>
2024-01-05 13:21:07 -0700

    calendar working! full cal is super cool!

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -28,9 +28,80 @@ const Calendar = () => {
         start: selected.startStr,
         end: selected.endStr,
         allDay: selected.allDay
-      })
+      });
     }
   }
+
+  const handleEventClick = (selected) => {
+    if (window.confirm(`Are you sure you want to delete the event '${selected.event.title}'`)) {
+      selected.event.remove();
+    }
+  }
+
+  return (
+    <Box m="20px">
+      <Header title="CALENDAR" subtitle="Full Calendar Interactive Page" />
+
+      <Box display="flex" justifyContent="space-between">
+        {/* Cal Side Bar */}
+        <Box flex="1 1 20%" backgroundColor={colors.primary[400]} p="15px" borderRadius="4px">
+          <Typography variant="h5">Events</Typography>
+          <List>
+            {currentEvents.map((event) => (
+              <ListItem
+                key={event.id}
+                sx={{backgroundColor: colors.greenAccent[500], margin: "10px 0", borderRadius: "2px"}}
+              >
+
+                <ListItemText
+                  primary={event.title}
+                  secondary={
+                    <Typography>
+                      {formatDate(event.start, {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric"
+                      })}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+
+        {/* Calendar */}
+        <Box flex="1 1 100%" ml="15px">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin
+            ]}
+            headerToolbar={{
+              left: "prev,next,today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth"
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            select={handleDateClick}
+            eventClick={handleEventClick}
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[
+              {id: "1234", title: "All Day Event", date: "2024-01-02"},
+              {id: "4321", title: "Timed Event", date: "2024-01-04"}
+            ]}
+          />
+        </Box>
+      </Box>
+    </Box>
+  )
 
 
 }
