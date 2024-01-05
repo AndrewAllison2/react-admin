a85b9b3355573e03ca8bb5e260c86533c432d6dc
Andrew Allison <andrewallison5211@gmail.com>
2024-01-05 12:53:56 -0700

    forgot an arrow function -_-

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { Calendar } from '@fullcalendar/core';
+// import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
@@ -8,28 +8,26 @@ import interactionPlugin from '@fullcalendar/interaction';
 import { Box, List, ListItem, ListItemText, Typography, useTheme } from "@mui/material";
 import Header from "../../components/Header.jsx";
 import { tokens } from "../../theme.js";
+import { formatDate } from '@fullcalendar/core';
+import FullCalendar from '@fullcalendar/react';
 
-const FullCalendar = () => {
-  {
-    plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin]
-    selectable: true
-  }
+const Calendar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode)
   const [currentEvents, setCurrentEvents] = useState([]);
 
-  const handleDateClick = (selected) = {
-    const title = prompt("Please enter a new title for your event")
-    const calendarApi = selected.view.Calendar
-    calendarApi.unselected()
+  const handleDateClick = (selected) => {
+    const title = prompt("Please enter a new title for your event");
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect();
 
     if(title) {
       calendarApi.addEvent({
         id: `${selected.dateStr}-${title}`,
         title,
-        start: selectedGridRowsCountSelector.startStr,
-        end: selectedGridRowsCountSelector.endStr,
-        allDay: selectedGridRowsCountSelector.allDay
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay
       })
     }
   }
@@ -37,4 +35,4 @@ const FullCalendar = () => {
 
 }
 
-export default FullCalendar;
+export default Calendar;
